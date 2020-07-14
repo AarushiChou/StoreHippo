33d47033da51cfc8e5fb2f49e634dc89c3f7856d
AarushiChou <67723702+AarushiChou@users.noreply.github.com>
2020-07-14 09:06:49 +0530

    Update Changingcontent.js

--- Changingcontent.js
+++ Changingcontent.js
@@ -6,7 +6,7 @@
 
 <p id="change">Using JavaScript to change html content .</p>
 
-<button type="button" onclick='document.getElementById("change").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+<button type="button" onclick='document.getElementById("change").innerHTML = "Hell JavaScript!"'>Click Me!</button>
 
 </body>
 </html>
